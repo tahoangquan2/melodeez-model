6ca80aed41e5f341dde6bb67d9ae6f5f15f7492c
tahoangquan2 <hoangquandt69@gmail.com>
2024-12-12 00:22:32 +0700

    Change training config

--- train_model_2.py
+++ train_model_2.py
@@ -9,7 +9,6 @@ def generate_lists(root_dir='output/output3', train_ratio=0.8, output_dir='check
     hum_files = glob.glob(os.path.join(root_dir, 'hum', '*.npy'))
     song_files = glob.glob(os.path.join(root_dir, 'song', '*.npy'))
 
-    # Extract unique IDs
     hum_ids = sorted(list(set([int(os.path.basename(f).split('.')[0].split('_')[0]) for f in hum_files])))
 
     num_train = int(len(hum_ids) * train_ratio)
@@ -19,7 +18,6 @@ def generate_lists(root_dir='output/output3', train_ratio=0.8, output_dir='check
     train_lines = []
     val_lines = []
 
-    # Process hum files
     for f in hum_files:
         filename = os.path.basename(f)
         file_id = int(filename.split('.')[0].split('_')[0])
@@ -30,7 +28,6 @@ def generate_lists(root_dir='output/output3', train_ratio=0.8, output_dir='check
         else:
             val_lines.append(line)
 
-    # Process song files
     for f in song_files:
         filename = os.path.basename(f)
         file_id = int(filename.split('.')[0].split('_')[0])
@@ -49,39 +46,19 @@ def generate_lists(root_dir='output/output3', train_ratio=0.8, output_dir='check
 
 class Config:
     def __init__(self):
-        # Training settings
         self.train_batch_size = 32
         self.num_workers = 4
-        self.max_epoch = 12
-        self.lr = 0.1
+        self.max_epoch = 50
+        self.lr = 3e-4
         self.weight_decay = 0.0005
-        self.lr_step = 10
         self.print_freq = 100
-        self.save_interval = 1
 
-        # Model settings
         self.backbone = 'resnet18'
-        self.use_se = True
-        self.metric = 'arc_margin'
-        self.easy_margin = False
-        self.loss = 'focal_loss'
-        self.optimizer = 'sgd'
-        # Number of unique songs
-        self.num_classes = 12
-
-        # Data settings
         self.input_shape = (1, 80, 630)
         self.train_root = 'output/output3'
         self.train_list = 'checkpoints/train_list.txt'
         self.val_list = 'checkpoints/val_list.txt'
-        self.mp3aug_ratio = 0.5
-        self.npy_aug = True
-
-        # Save settings
         self.checkpoints_path = 'checkpoints'
-        # Disable visualization
-        self.display = False
-
 
 def main():
     opt = Config()
@@ -93,7 +70,6 @@ def main():
 
     from train_model_1 import train_model_1
     train_model_1(opt)
-
 
 if __name__ == '__main__':
     main()
